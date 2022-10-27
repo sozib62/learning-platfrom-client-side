3ad7ba205bed5e41d3fdce9a3856e4bb2cf52064
sozib62 <webdevsozib62@gmail.com>
2022-10-27 17:24:56 +0600

    added 4th que

--- src/Components/Component/Blog/Blog.js
+++ src/Components/Component/Blog/Blog.js
@@ -15,6 +15,10 @@ const Blog = () => {
                 <h1 className='text-2xl mb-2'>Q: How does the private route work?</h1>
                 <p className='text-lg'> Ans: The private route component is similar to the public route, the only change is that redirect URL and authenticate condition. If the user is not authenticated he will be redirected to the login page and the user can only access the authenticated routes If he is authenticated (Logged in). Public and Private routes will also restrict accessing the previously visited routes using the browser back button after logout. I hope you have found this useful. Thank you for reading.</p>
             </div>
+            <div className='mt-8 p-4 bg-zinc-300 drop-shadow-2xl'>
+                <h1 className='text-2xl mb-2'>Q: What is Node? How does Node work?</h1>
+                <p className='text-lg'> Ans: Node. js is a JavaScript runtime environment that achieves low latency and high throughput by taking a “non-blocking” approach to serving requests. In other words, Node. js wastes no time or resources on waiting for I/O requests to return.In the traditional approach to creating web servers, for each incoming request or connection the server spawns a new thread of execution or even forks a new process to handle the request and send a response. Conceptually, this makes perfect sense, but in practice it incurs a great deal of overhead.</p>
+            </div>
         </div>
     );
 };
