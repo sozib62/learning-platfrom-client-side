5ef4224356f4d03459c5fcb9aa21cd071f9875d8
sozib62 <webdevsozib62@gmail.com>
2022-10-27 17:50:27 +0600

    added favicon

--- src/Components/Component/Courses/Courses.js
+++ src/Components/Component/Courses/Courses.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import CourseDisplay from '../CourseDisplay/CourseDisplay';
+
 import PrivateCourse from '../privateCourse/PrivateCourse';
 
 
@@ -8,7 +8,7 @@ const Courses = () => {
     const course = useLoaderData()
 
     return (
-        <div className=' grid grid-cols-3 gap-10 ml-48 mt-20 '>
+        <div className=' grid grid-cols-3 sm:grid-cols-1 gap-10 ml-48 mt-20 '>
             {
                 course.map(cor => <PrivateCourse
                     key={cor.id}
